47c295deae54855dda62070d9e7af3cfd738f7fd
François Lion <francois.lion1805@gmail.com>
2021-11-18 15:46:17 +0100

    change: reponse exercice 6

--- reponses/exercice6/exercice6.sql
+++ reponses/exercice6/exercice6.sql
@@ -1,2 +1,12 @@
 SELECT NUMERO_TICKET AS 'Tickets du mois mars et avril' FROM ticket
 WHERE DATE_VENTE BETWEEN '2014-03-01' AND '2014-04-30';
+
+
+SELECT NUMERO_TICKET AS 'Tickets du mois mars et avril' 
+FROM 
+    ticket
+WHERE
+    annee = 2014
+AND 
+    MONTH(date_vente) IN (3, 4)
+;
