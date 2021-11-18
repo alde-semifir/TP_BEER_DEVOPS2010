37071e9699bd2991345d5658ae0a4624c71ad863
Tommy Tumba <tommy@tumba.fr>
2021-11-18 16:49:36 +0100

    Feat: Exercice 12 résolu

---
+++ Exercice12/exercice12.sql
@@ -0,0 +1,17 @@
+# 12. Lister chaque ticket pour lequel la quantité totale d’articles vendus est supérieure à
+# 500. On exclura du total, les vente s ayant une quantité supérieure à 50 (classer par
+# quantité décroissante)
+
+SELECT 
+    ventes.NUMERO_TICKET 'Numéro Ticket',
+    SUM(CASE
+        WHEN ventes.QUANTITE > 50 THEN ventes.QUANTITE
+        ELSE 0
+    END) as Total
+FROM
+    ventes
+GROUP BY ventes.NUMERO_TICKET
+HAVING Total > 0
+ORDER BY Total DESC
+
+
