843582100b7f8e4e87c0082f4e96299c2ffbe720
Randy <randy.fauchoit@gmail.com>
2021-11-18 13:58:06 +0100

    feat: reponse exercice 8

--- TP_BEER_DEVOPS2010/Réponses/REPONSE 8.sql
+++ TP_BEER_DEVOPS2010/Réponses/REPONSE 8.sql
@@ -1,4 +1,4 @@
 SELECT NOM_ARTICLE, ID_ARTICLE,  couleur.NOM_COULEUR as couleur
 FROM article
-JOIN couleur on couleur.ID_Couleur = couleur.ID_Couleur
+JOIN couleur on couleur.ID_Couleur = article.ID_Couleur
 ORDER BY couleur
