67de5c3d988341d40ece9c3d4a90da03e0e64575
Randy <randy.fauchoit@gmail.com>
2021-11-19 10:43:56 +0100

    feat : Reponse 16

---
+++ TP_BEER_DEVOPS2010/Réponses/REPONSE 16.sql
@@ -0,0 +1,4 @@
+SELECT ventes.ANNEE, ventes.NUMERO_TICKET, round(SUM((ventes.QUANTITE * article.PRIX_ACHAT) * 1.15),2) as CA
+FROM ventes
+JOIN article ON ventes.ID_ARTICLE = article.ID_ARTICLE
+GROUP BY ventes.NUMERO_TICKET;
